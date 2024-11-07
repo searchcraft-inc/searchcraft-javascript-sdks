79ff4ae828db4b689de11d7a5694084a245dff50
michaelyons <lyons@madebychalk.com>
2024-11-07 15:28:05 -0700

    Update Results error message

--- packages/react-sdk/src/components/ui/BaseSearchResults/index.tsx
+++ packages/react-sdk/src/components/ui/BaseSearchResults/index.tsx
@@ -4,37 +4,37 @@ import styles from '@styles/sc-search-results.module.scss';
 
 import { useSearchcraft } from '@/components/providers/SearchcraftProvider';
 import BaseSearchResult from '@components/ui/BaseSearchResult';
+import ErrorMessage from '@components/ui/components/ErrorMessage';
 
 const BaseSearchResults: FC = () => {
-  const { searchResults } = useSearchcraft();
+  const { searchResults, query } = useSearchcraft();
 
   return (
     <div className={styles.resultsContainer}>
-      {searchResults?.data?.hits ? (
-        searchResults?.data?.hits?.map((document, index) => {
-          const { doc: result } = document;
-          const callback = () => {
-            console.log('interactive');
-          };
-          const buttonCallback = () => {
-            console.log('button callback');
-          };
-          return (
-            <BaseSearchResult
-              key={`${result?.id}-${index}`}
-              buttonLabel='View More'
-              buttonCallbackFn={buttonCallback}
-              callbackFn={callback}
-              interactiveResult
-              imageSrc={result?.poster}
-              resultBodyContent={result?.overview}
-              resultHeading={result?.title}
-              resultSubheading={result?.release_date}
-            />
-          );
-        })
-      ) : (
-        <div>no results found</div>
+      {searchResults?.data?.hits?.map((document, index) => {
+        const { doc: result } = document;
+        const callback = () => {
+          console.log('interactive');
+        };
+        const buttonCallback = () => {
+          console.log('button callback');
+        };
+        return (
+          <BaseSearchResult
+            key={`${result?.id}-${index}`}
+            buttonLabel='View More'
+            buttonCallbackFn={buttonCallback}
+            callbackFn={callback}
+            interactiveResult
+            imageSrc={result?.poster}
+            resultBodyContent={result?.overview}
+            resultHeading={result?.title}
+            resultSubheading={result?.release_date}
+          />
+        );
+      })}
+      {query.length > 0 && searchResults?.data?.hits?.length === 0 && (
+        <ErrorMessage errorMessage={`No Results found for ${query} query`} />
       )}
     </div>
   );
