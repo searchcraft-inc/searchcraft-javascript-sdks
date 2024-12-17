6a41030c14a4d2adc0f04d0fe86b1c68fd3f00f9
michaelyons <lyons@madebychalk.com>
2024-12-17 11:54:33 -0700

    Update build and bump version

--- packages/javascript-sdk/www/build/index.esm.js
+++ packages/javascript-sdk/www/build/index.esm.js
@@ -1,15 +1,2 @@
-export { e as extractDynamicProperties, p as parseCustomStyles, a as parseSearchKeys, s as serializeStyles } from './utils-dbb6302b.js';
-export { u as useSearchcraftStore, a as useThemeStore } from './store-0451a982.js';
-import './_commonjsHelpers-63cbe26c.js';
-
-/**
- * @fileoverview entry point for your component library
- *
- * This is the entry point for your component library. Use this file to export utilities,
- * constants or data structure that accompany your components.
- *
- * DO NOT use this file to export your components. Instead, use the recommended approaches
- * to consume components of this package as outlined in the `README.md`.
- */
-
+export{e as extractDynamicProperties,p as parseCustomStyles,a as parseSearchKeys,s as serializeStyles}from"./p-ff6c3650.js";export{u as useSearchcraftStore,a as useThemeStore}from"./p-cd489e98.js";
 //# sourceMappingURL=index.esm.js.map
