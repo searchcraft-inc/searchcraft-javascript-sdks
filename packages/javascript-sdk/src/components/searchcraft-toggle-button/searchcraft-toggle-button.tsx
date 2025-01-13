1e3e3dcdf8fc3c422ac6615bd60a3a0dbf1ebae2
Travis Rother <trother@searchcraft.io>
2025-01-13 16:14:45 -0700

    Updates filter panel toggle button behavior re:SC-562,SC-576

--- packages/javascript-sdk/src/components/searchcraft-toggle-button/searchcraft-toggle-button.tsx
+++ packages/javascript-sdk/src/components/searchcraft-toggle-button/searchcraft-toggle-button.tsx
@@ -36,10 +36,10 @@ export class SearchcraftToggleButton {
   connectedCallback() {
     /** When the query changes, sets toggle button state back to inactive. */
     this.unsubscribe = useSearchcraftStore.subscribe((state) => {
-      if (state.query !== this.lastQuery) {
+      if (state.query !== this.lastQuery && state.query.trim().length === 0) {
         this.isActive = false;
-        this.lastQuery = state.query;
       }
+      this.lastQuery = state.query;
     });
   }
 
