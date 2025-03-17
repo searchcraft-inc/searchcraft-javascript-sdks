2ac82ead336f6cedb838a92a1b11f5c8b0d22f5b
Travis Rother <trother@searchcraft.io>
2025-03-17 15:52:05 -0600

    Adds nextjs to compendium for sdk SSR support re:SC-1070
    - Adds nextjs to compendium, so that components can be previewed in an environment that uses ssr.
    - Adds ssr output target to stencil config
    
    SSR support wip
    
    Adds nextjs to compendium re:SC-1070

--- packages/javascript-sdk/stencil.config.ts
+++ packages/javascript-sdk/stencil.config.ts
@@ -20,23 +20,24 @@ export const config: Config = {
   validatePrimaryPackageOutputTarget: true,
   outputTargets: [
     reactOutputTarget({
-      // Relative path to where the React components will be generated
       outDir: '../react-sdk/src/stencil-web-components/',
       stencilPackageName: '@searchcraft/javascript-sdk',
     }),
     vueOutputTarget({
-      // The proxiesFile is the relative path to the file that will be generated with all the Vue component wrappers.
       proxiesFile: '../vue-sdk/src/stencil-web-components.ts',
       componentCorePackage: '@searchcraft/javascript-sdk',
       includePolyfills: false,
     }),
     {
       type: 'dist-custom-elements',
-      dir: 'dist/components', // Ensures .js files are generated here
+      dir: 'dist/components',
       includeGlobalScripts: false,
       customElementsExportBehavior: 'single-export-module',
       externalRuntime: false,
       isPrimaryPackageOutputTarget: true,
+    },
+    {
+      type: 'dist-hydrate-script',
     },
     {
       type: 'docs-readme',
