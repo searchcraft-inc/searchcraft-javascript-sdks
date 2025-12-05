1b181ec43d23ff89b999bd3fc881865e75e0e885
Don MacKinnon <dmackinnon@searchcraft.io>
2025-12-05 12:33:49 -0700

    Compendium storybook upgrades

--- packages/compendium/storybook/configs/vue-config/main.ts
+++ packages/compendium/storybook/configs/vue-config/main.ts
@@ -1,6 +1,10 @@
-import path, { resolve } from 'node:path';
 import type { StorybookConfig } from '@storybook/vue3-vite';
 import vue from '@vitejs/plugin-vue';
+import path, { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const config: StorybookConfig = {
   stories: [
